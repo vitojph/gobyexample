3888f8702446683e15f9392e1ca262d075085aea
Victor Peinado <vitojph@gmail.com>
2015-02-09 23:35:30 +0100

    minor changes in time managing

--- time.go
+++ time.go
@@ -5,13 +5,22 @@ import (
     "time"
 )
 
+
 func main() {
     p := fmt.Printf
     now := time.Now()
+    findes := map[string]int{"Saturday": 1, "Sunday": 1}
+    //vacaciones := map[string]int{"July": 1, "August": 1}
+
+    month := fmt.Sprintf("%s", now.Month())
+    _, ok := findes[month]
+    fmt.Println("está:", ok)
+
 
     p("Hora %d\n", now.Hour())
     p("Minutos %d\n", now.Minute())
     p("Día de la semana %s\n", time.Weekday(now.Weekday()))
+    p("Día de la semana %s\n", now.Weekday())
     p("Día %d\n", now.Day())
     p("Mes %d\n", now.Month())
     p("Año %d\n", now.Year())
@@ -28,5 +37,9 @@ func main() {
     //t, _ := time.Parse(longForm, date)
     //  fmt.Println(t.Local())
 
+    fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(false || false)
 
 }
